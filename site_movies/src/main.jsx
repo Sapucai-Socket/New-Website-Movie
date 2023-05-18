781d648f96b18f8acbdf5702bb00d7513de14d0c
lsaturnino <lucas.saturnino.oliveira10@aluno.ifce.edu.br>
2023-05-17 21:17:01 -0300

    Organizando components e pages

--- site_movies/src/main.jsx
+++ site_movies/src/main.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import App from './App'
+import Home from './pages/Home/Home'
+
+import './pages/Home/Home.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter >
+      <Routes>
+        <Route element={<App />}>
+          <Route path='/' element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
---
+++ site_movies/src/pages/Home/Home.jsx
@@ -0,0 +1,8 @@
+import { useState, useEffect } from "react"
+
+const Home = () => {
+    <div>dhdhhd</div>
+
+}
+
+export default Home;
