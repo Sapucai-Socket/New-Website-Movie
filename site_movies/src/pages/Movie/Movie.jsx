d9a2929e0c8c1f351adb2d8ace87d3fb0b6ce3b4
Henrique Abner <102832927+HenriqueAbn@users.noreply.github.com>
2023-05-26 17:04:17 -0300

    Sombra na Img em MoviePage

--- site_movies/src/pages/Movie/Movie.jsx
+++ site_movies/src/pages/Movie/Movie.jsx
@@ -37,38 +37,40 @@ const Movie = () => {
         <>
           <MovieCard movie={movie} showLink={false} type={1} />
           <div className="container">
-            <img id="movieBackground" src={imageUrl + movie.backdrop_path} alt={movie.title} />
-            <div id="infoPanel" className="infoPanel">
-              <ol id="infoList">
-                <li>
-                  <img id="movieImg" src={imageUrl + movie.poster_path} alt={movie.title} />
-                </li>
-                <li>
-                  <ol>
-                    <div className="headMovie">
-                      <p id="movieTitle" className="Title">{movie.title}</p>
-                      <p id="movieDirector" className="Director">{movie.director}</p>
-                      <p id="movieTagline" className="tagline">{movie.tagline}</p>
-                    </div>
-                    <div id="description" className="info description">
-                      <p>{movie.overview}</p>
-                    </div>
-                    <div id="duration">
-                      <p>{movie.runtime} minutos de duração.</p>
-                    </div>
-                  </ol>
-                </li>
-                <li>
-                  <div className="movieActions">
-                    <button id="likeAdd">❤</button>
-                    <button id="watchlistAdd">📃</button>
+
+          <img id="movieBackground" src={imageUrl + movie.backdrop_path} alt={movie.title} />
+          <div className="backgroundMovieShadow"></div>
+          <div id="infoPanel" className="infoPanel">
+            <ol id="infoList">
+              <li>
+                <img id="movieImg" src={imageUrl + movie.poster_path} alt={movie.title} />
+              </li>
+              <li>
+                <ol>
+                  <div className="headMovie">
+                    <p id="movieTitle" className="Title">{movie.title}</p>
+                    <p id="movieDirector" className="Director">{movie.director}</p>
+                    <p id="movieTagline" className="tagline">{movie.tagline}</p>
                   </div>
-                </li>
-              </ol>
-
-            </div>
-
+                  <div id="description" className="info description">
+                    <p>{movie.overview}</p>
+                  </div>
+                  <div id="duration">
+                    <p>{movie.runtime} minutos de duração.</p>
+                  </div>
+                </ol>
+              </li>
+              <li>
+                <div className="movieActions">
+                  <button id="likeAdd">❤</button>
+                  <button id="watchlistAdd">📃</button>
+                </div>
+              </li>
+            </ol>
+            
           </div>
+          
+        </div>
         </>
       )}
     </div>
