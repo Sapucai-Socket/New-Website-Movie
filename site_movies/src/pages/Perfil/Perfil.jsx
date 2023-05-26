d2cdd37f31aef43b61e20452449ef0e8bb1da72f
Válcler Manoel Vieira Camelo Maia <valcler.manoel.vieira06@aluno.ifce.edu.br>
2023-05-25 22:38:51 -0300

    Tela de perfil com leitura dos dados do usuário

--- site_movies/src/pages/Perfil/Perfil.jsx
+++ site_movies/src/pages/Perfil/Perfil.jsx
@@ -1,25 +1,25 @@
 import React, { useState, useEffect } from "react";
 import "./Perfil.css";
 import { useNavigate } from "react-router-dom";
-import { auth } from "../../firebase";
-import { onAuthStateChanged } from "firebase/auth";
+import { auth, db } from "../../firebase";
+import { onAuthStateChanged, signInWithPopup, signOut, } from "firebase/auth";
 import { collection, getDocs } from "firebase/firestore";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-import { db } from "../../firebase";
 import { doc, getDoc } from "firebase/firestore";
-
 
 const Perfil = () => {
     const [authUser, setAuthUser] = useState(null);
-    const [nome, setNome] = useState('');
-    const [descricao, setDescricao] = useState('');
+    const [nome, setNome] = useState("");
+    const [descricao, setDescricao] = useState("");
     const navigate = useNavigate();
 
     const getNomeUsuario = async (uid) => {
         const docRef = doc(db, "users", uid);
         const docSnap = await getDoc(docRef);
         const name = docSnap.data().nome;
-        setNome(name)
+        setNome(name);
         /*
                 if (docSnap.exists()) {
                     console.log("Nome de usuário:", docSnap.data().nome);
@@ -28,46 +28,66 @@ const Perfil = () => {
                     console.log("No such document!");
                 }
                 */
-    }
+    };
 
     const getDescricao = async (uid) => {
         const docRef = doc(db, "users", uid);
         const docSnap = await getDoc(docRef);
         const descricao = docSnap.data().descricao;
-        setDescricao(descricao)
-    }
+        setDescricao(descricao);
+    };
 
     useEffect(() => {
-        onAuthStateChanged(auth, (user) => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
             if (user) {
                 const uid = user.uid;
-                setAuthUser(uid);
+                setAuthUser(user);
                 getNomeUsuario(uid);
                 getDescricao(uid);
             } else {
-                navigate('/login');
+                navigate("/login");
             }
         });
-    });
 
+        return () => {
+            unsubscribe();
+        };
+    }, []);
 
+    const handleLogout = async () => {
+        try {
+            await signOut(auth);
+            navigate("/");
+            toast.success("Usuário desconectado");
+        } catch (error) {
+            console.log("Erro ao fazer logout:", error);
+        }
+    };
 
-
+    const handleGoogleLogin = async () => {
+        try {
+            await signInWithPopup(auth, googleAuthProvider);
+        } catch (error) {
+            console.log("Erro ao fazer login com o Google:", error);
+        }
+    };
 
     return (
         <div>
             <div className="profileContainer">
                 <div className="pfpImage">
-                    <img
-                        id="pfp"
-                        src="https://pbs.twimg.com/profile_images/836420807085674497/fjDNEUsp_400x400.jpg"
-                    />
+                    {authUser?.photoURL ? (
+                        <img id="pfp" src={authUser.photoURL} alt="Perfil" />
+                    ) : (
+                        <img src="/iconepadrao.png" alt="png" id="userPadrao" />
+                    )}
                 </div>
                 <div>
-                    <h1 id="nomeUsuario">{nome}</h1>
+                    <h1 id="nomeUsuario">{!authUser?.displayName ? nome : authUser.displayName}</h1>
                     <i id="descUsuario">{descricao}</i>
                 </div>
             </div>
+
             <div className="otherUserInfos">
                 <div className="otherUserInfo">
                     <span className="quantity">4</span>
@@ -102,6 +122,7 @@ const Perfil = () => {
                     <span className="parameter">SEGUIDORES</span>
                 </div>
             </div>
+            <button onClick={handleLogout}>Sair</button>
             <div id="favoritos">
                 <h2 id="favoritos-cabecalho">FILMES FAVORITOS</h2>
 
@@ -111,6 +132,7 @@ const Perfil = () => {
                             <img
                                 className="filme-favorito"
                                 src="https://m.media-amazon.com/images/M/MV5BNzQzMzJhZTEtOWM4NS00MTdhLTg0YjgtMjM4MDRkZjUwZDBlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg"
+                                alt="Filme Favorito"
                             />
                         </a>
                     </div>
@@ -120,6 +142,7 @@ const Perfil = () => {
                             <img
                                 className="filme-favorito"
                                 src="https://m.media-amazon.com/images/M/MV5BY2NkZjEzMDgtN2RjYy00YzM1LWI4ZmQtMjIwYjFjNmI3ZGEwXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_FMjpg_UX1000_.jpg"
+                                alt="Filme Favorito"
                             />
                         </a>
                     </div>
@@ -128,6 +151,7 @@ const Perfil = () => {
                             <img
                                 className="filme-favorito"
                                 src="https://image.tmdb.org/t/p/original/gmU7P3FzGFsl2wiSDhx9znZCNub.jpg"
+                                alt="Filme Favorito"
                             />
                         </a>
                     </div>
@@ -141,6 +165,7 @@ const Perfil = () => {
                         <img
                             className="filme-avaliado"
                             src="https://image.tmdb.org/t/p/original/MbP1pIUKQcZaC1XCwSomuiLrva.jpg"
+                            alt="Filme Avaliado"
                         />
                     </a>
                 </div>
@@ -156,8 +181,9 @@ const Perfil = () => {
                     </span>
                 </div>
             </div>
+
         </div>
     );
-}
+};
 
 export default Perfil;
